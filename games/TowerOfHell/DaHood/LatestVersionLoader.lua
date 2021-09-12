1040d906e41f9724771e40276e1b4e199f5c4b6b
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-09-12 17:49:38 -0400

    Create LatestVersionLoader.lua

---
+++ games/TowerOfHell/DaHood/LatestVersionLoader.lua
@@ -0,0 +1,4 @@
+local inBeta = tonumber(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/inBeta")) == 1 and true or false
+local version = game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/stable_verson")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/releases/"..((inBeta and "beta") or "")..version..".lua"))()
