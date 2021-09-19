12ecc040e29fc33e20bf978bb7da73270bcd2e82
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-09-19 19:19:09 -0400

    Create top-bar-update.lua

---
+++ dump/top-bar-update.lua
@@ -0,0 +1,11 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Scripts8/main/topbar-loader.lua"))()
+
+--[[
+
+Copy and paste into script using Topbar-Loader
+
+Index the Topbar class like so:
+
+local topbar = shared.topbar
+
+]]--
