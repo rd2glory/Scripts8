419fa244955c8d0dd9946ec6b6ea7a0f79613ec5
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-09-25 17:06:13 -0400

    Updated Full Version Loading

--- games/DaHood/LatestVersionLoader.lua
+++ games/DaHood/LatestVersionLoader.lua
@@ -1,4 +1,4 @@
 local inBeta = tonumber(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/inBeta")) == 1 and true or false
 local version = game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/stable_verson")
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/releases/"..((inBeta and "beta") or "")..version..".lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/releases/"..((inBeta and "beta") or "v")..version..".lua"))()
