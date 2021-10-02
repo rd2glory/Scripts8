2450cb6297618bdad55dba46afc4c5c133eeb09d
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-10-02 13:22:25 -0400

    Create BetaLoader.lua

---
+++ games/DaHood/BetaLoader.lua
@@ -0,0 +1,3 @@
+local version = game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/latest_beta")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/releases/".."beta"..version..".lua"))()
