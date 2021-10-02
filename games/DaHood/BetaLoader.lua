8adc8e878c1aa97c424f1bcf590456e8e0486380
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-10-02 13:32:06 -0400

    Update BetaLoader.lua

--- games/DaHood/BetaLoader.lua
+++ games/DaHood/BetaLoader.lua
@@ -1,3 +1,3 @@
-local version = game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/latest_beta")
+local version = tonumber(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/latest_beta"))
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Artemis/main/releases/".."beta"..version..".lua"))()
