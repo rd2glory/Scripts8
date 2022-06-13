71a7748f5aed3577cefcdcbfb8132b0c2c1ea55c
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2022-06-13 19:12:16 -0400

    autoexec

--- games/Jailbreak/autoexec.lua
+++ games/Jailbreak/autoexec.lua
@@ -1,10 +1,10 @@
-local lastSafeVersion = tonumber(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Scripts8/main/games/Jailbreak/LastSafeVersion"))
+local lastSafeVersion = tonumber(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Scripts8/main/games/Jailbreak/LastSafeVersion")) or -1
 
 if game.PlaceId == 606849621 or game.PlaceId == 9780994092 then
-  if game.PlaceVersion > lastSafeVersion then
-    warn("Jailbreak+ has not been tested with this version of Jailbreak, please contact the dev in order to test for any possible patches")
-  else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Scripts8/main/games/Jailbreak/JailbreakPlus.lua"))()
-  end
+	if game.PlaceVersion > lastSafeVersion then
+		warn("Jailbreak+ has not been tested with this version of Jailbreak, please contact the dev in order to test for any possible patches")
+	else
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Scripts8/main/games/Jailbreak/JailbreakPlus.lua"))()
+	end
 	task.wait(9e9)
 end
