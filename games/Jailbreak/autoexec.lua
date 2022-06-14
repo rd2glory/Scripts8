55f7c481e86b8df3344ed9922438767a1a4cd64e
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2022-06-14 19:26:43 -0400

    Indentation

--- games/Jailbreak/autoexec.lua
+++ games/Jailbreak/autoexec.lua
@@ -5,7 +5,7 @@ if game.PlaceId == 606849621 or game.PlaceId == 9780994092 then
 	if game.PlaceVersion > lastSafeVersion then
 		warn("Jailbreak+ has not been tested with this version of Jailbreak, please contact the dev in order to test for any possible patches")
 	else
-	    print("Place version ("..game.PlaceVersion..") is safe for launching JB+")
+		print("Place version ("..game.PlaceVersion..") is safe for launching JB+")
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Scripts8/main/games/Jailbreak/JailbreakPlus.lua"))()
 	end
 	task.wait(9e9)
