dab9bba11d565022b0f4435f4740099513c3aa71
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2022-06-14 23:48:08 -0400

    format

--- games/Jailbreak/autoexec.lua
+++ games/Jailbreak/autoexec.lua
@@ -3,7 +3,7 @@ local lastSafeVersion = tonumber(game:HttpGet("https://raw.githubusercontent.com
 if game.PlaceId == 606849621 or game.PlaceId == 9780994092 then
 	repeat game:GetService("RunService").RenderStepped:Wait() until game.PlaceVersion ~= 0
 	if game.PlaceVersion > lastSafeVersion then
-		warn("Jailbreak+ has not been tested with this version of Jailbreak, please contact the dev in order to test for any possible patches (Last tested for place version "..lastSafeVersion..", current is "..tostring(game.PlaceVersion))
+		warn("Jailbreak+ has not been tested with this version of Jailbreak, please contact the dev in order to test for any possible patches (Last tested for place version "..lastSafeVersion..", current is "..tostring(game.PlaceVersion)..")")
 	else
 		print("Place version ("..game.PlaceVersion..") is safe for launching JB+")
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Scripts8/main/games/Jailbreak/JailbreakPlus.lua"))()
