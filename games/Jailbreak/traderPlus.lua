1961016b96a945425803e7e1fabd6193a233d5bd
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2022-07-10 14:08:31 -0400

    v9
    updated theme

--- games/Jailbreak/traderPlus.lua
+++ games/Jailbreak/traderPlus.lua
@@ -8,7 +8,7 @@ local HB = game:GetService("RunService").Heartbeat
 local player = Players.LocalPlayer
 
 -- init
-local defaultTheme = "Sentinel"
+local defaultTheme = "Midnight"
 local trader = Library.CreateLib("Trader+", defaultTheme)
 
 -- variables
